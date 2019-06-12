5de355f70a27d8842b6cb8e1bba80fff8ca583fb
alejandro.castrillon <alejandro.castrillon@utp.edu.co>
2019-06-12 18:02:25 -0500

    Ajuste

---
+++ TransitoFront/src/app/model/Sugerencia.model.ts
@@ -0,0 +1,6 @@
+export class Sugerencia {
+    public latitud: number;
+    public longitud: number;
+    public intensidadLluvia: string;
+    public estadoCarretera: string;
+}
--- TransitoFront/src/app/sugerencia/dialog-formulario-sugerencia/dialog-formulario-sugerencia.component.ts
+++ TransitoFront/src/app/sugerencia/dialog-formulario-sugerencia/dialog-formulario-sugerencia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/api';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Sugerencia } from 'src/app/model/Sugerencia.model';
 
 @Component({
   selector: 'app-dialog-formulario-sugerencia',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogFormularioSugerenciaComponent implements OnInit {
 
-  constructor() { }
+  sugerencia: Sugerencia;
+  sugerenciaForm: FormGroup
+
+  constructor(private dialogRef: DynamicDialogRef, private data: DynamicDialogConfig, private formBuilder: FormBuilder) {
+    this.sugerencia.latitud = data.data.latitud;
+    this.sugerencia.longitud = data.data.longitud;
+  }
 
   ngOnInit() {
+    this.validateForm();
+  }
+
+  public validateForm(): void {
+    this.sugerenciaForm = this.formBuilder.group({
+      'latitud': [this.sugerencia.latitud, [
+        Validators.required
+      ]],
+      'longitud': [this.sugerencia.longitud, [
+        Validators.required
+      ]],
+      'intensidadLLuvia': [this.sugerencia.intensidadLluvia, [
+        Validators.required
+      ]],
+      'estadoCarretera': [this.sugerencia.estadoCarretera, [
+        Validators.required
+      ]]
+    });
   }
 
 }
